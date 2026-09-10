cb90a498f98c3acf6ec83b0745ec5080780b78a4
Tejpalsinh32 <mr.tejpalsinh001@gmail.com>
2026-09-10 09:31:34 +0530

    variable and datatypes added

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountId = 10001
+let acccountEmail = "paltejpalsinh23@gmail.com"
+var accountPassword = "2345"
+accountCity = "Ahmedabad"
+
+// accountId = 2
+console.log(accountId);
+console.table([accountId,acccountEmail,accountPassword,accountCity]);
