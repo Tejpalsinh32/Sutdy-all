fd29b8858f801ff7a44cd4f162d58414e4b02bd3
Tejpalsinh32 <mr.tejpalsinh001@gmail.com>
2026-09-11 10:00:37 +0530

    datatypes-summary

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // "33" => 33
 // "33abc" => NaN
@@ -13,8 +13,8 @@ console.log(valueInNumber);
 
 let isLoggedIn = 1
 
-let booleanIsloggedIn = Boolean(isLoggedIn)
-console.log(booleanIsloggedIn);
+// let booleanIsloggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsloggedIn);
 
 // 1 => ture; 0 =>bfalse
 // "" => fasle
@@ -23,5 +23,35 @@ console.log(booleanIsloggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ******************************************** Opearations ****************************************
+
+let value =3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+let str1 = "hello"
+let str2 = " tejpalsinh"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+ "2");
+// console.log("1"+ "2");
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null >=0);
+
+// ===
+ 
---
+++ datatypes-summary.js
@@ -0,0 +1,27 @@
+// primitive 
+// 7 types : String, Number , booleran , Null , undefinde , Symbol , BigInt
+
+// reference (Non primitive)
+// Array, Objects, Functions
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+// const id = Symbol('123')
+// const anotherIn = Symbol('123')
+
+// console.log(id === anotherIn);
+
+const heros = ["shaktiman","naaagraj","doga"];
+let myObj ={
+    name: "Tejpaliisnh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("hello world");
+}
+console.log(typeof heros);
