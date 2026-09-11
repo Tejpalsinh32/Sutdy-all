3dd045908876f2e4e17d5b7240cdb62311411acb
Tejpalsinh32 <mr.tejpalsinh001@gmail.com>
2026-09-11 11:05:39 +0530

    added String

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+ // Stack (Primitive), Heaap (Non-Primiver)
+ let myYoutubename = "Tejpalsinh"
+
+ let anothername = myYoutubename
+ anothername = "chaiaurcode"
+
+ console.log(anothername);
+  console.log(myYoutubename);
+
+  let userOne ={
+    email: "user@google.com",
+    upi: "user"
+  }
+
+  let usertwo = userOne
+
+  usertwo.email = "tejpal@gmail.com"
+
+  console.log(userOne.email)
+  console.log(usertwo.email)
---
+++ 01_basics/05_Strings.js
@@ -0,0 +1,30 @@
+const name = "tejpalsinh"
+const repoCount = 50
+
+// console.log(name +repoCount + "Value");
+console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Tejpalsinh')
+console.log(gameName);
+// console.log(gameName[gameName]);
+// console.log(gameName[gameName]);
+// console.log(gameName[gameName]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('j'));
+
+const newString = gameName.substring(0,4)
+console.log(newString);
+
+const anotherString = gameName.slice(0,4)
+console.log(anotherString);
+
+const newStringOne = " tejpalsinh "
+console.log(newStringOne);
+console.log(newStringOne.trim());
+
+const url = "https://www.youtube.com/watch?v=sscX432bMZo"
+console.log(url.replace('432','-'))
+
+url.includes( )
