d2d1ea124ae19fa6f2f34e60cb48f1044e62cd2a
Tejpalsinh32 <mr.tejpalsinh001@gmail.com>
2026-09-11 14:44:42 +0530

    all Done

---
+++ 01_basics/07_datesinJs.js
@@ -0,0 +1,25 @@
+let myDate = new Date()
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleString());
+
+// let myCreateDate = new Date(2023,0,23)
+// console.log(myCreateDate.toDateString());
+let myCreateDate = new Date("2023-01-14")
+// console.log(myCreateDate.toLocaleString());
+let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreateDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+ 1);
+// console.log(newDate.getDay());
+
+newDate.toLocaleString('default',{
+    weekday: "long"
+})
+
+console.log(newDate);
